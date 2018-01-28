4ac22fe6d7244209ac4b4d7dea851e3923e31d82
devxris <chuwei0915@gmail.com>
2018-01-28 21:50:19 +0800

    Initial Commit

---
+++ Headlines/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Headlines
+//
+//  Created by Chris Huang on 28/01/2018.
+//  Copyright © 2018 Chris Huang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
